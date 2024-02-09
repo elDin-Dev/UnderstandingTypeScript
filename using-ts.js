cf33cec4acbc0e9f1716ca52db2c7a1f2cba75f7
elDin-Dev <alexmaildese-dev@yahoo.com>
2024-02-09 17:55:40 +0100

    compiling to js from ts

--- using-ts.js
+++ using-ts.js
@@ -5,5 +5,5 @@ function add(num1, num2) {
     return num1 + num2;
 }
 button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
-    console.log(add(input1 === null || input1 === void 0 ? void 0 : input1.value, input2.value));
+    console.log(add(+input1.value, +input2.value));
 });
