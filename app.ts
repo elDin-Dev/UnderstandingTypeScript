30338fc31081374489deb1433c0a3612f3738e4d
elDin-Dev <alexmaildese-dev@yahoo.com>
2024-02-19 17:31:32 +0100

    tuples

--- app.ts
+++ app.ts
@@ -3,14 +3,18 @@ const person: {
     // here is a kind of the interface of the object
     name: string,
     age: number,
-    hobbies: string[];
+    hobbies: string[],
+    role: [number, string] //this is a tuple
 } = {
     name: "alex",
     age: 48,
-    hobbies: ['Sports','Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'developer']
 }
 
 console.log(person.name);
+
+console.log(person.role);
 
 for (const hobby in person.hobbies){
     console.log(hobby.toUpperCase);
