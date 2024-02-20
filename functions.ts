26f84cbe773875f25fb45a461c3e88f90dd5d804
elDin-Dev <alexmaildese-dev@yahoo.com>
2024-02-20 18:03:52 +0100

    initialize ts compiler

---
+++ functions.ts
@@ -0,0 +1,25 @@
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+function printResult(n: number):void {
+    console.log("Result: " + n);
+}
+
+printResult(add(8, 2));
+
+
+// combineValues could hold a pointer to any funciton 
+// that takes no paramters a returns a number.
+let combineValues: () => number;
+
+function addAndHanble(n: number, n2: number, cb: (n:number) => void) {
+    const result = n + n2;
+    
+    cb(result);
+    return result;
+}
+
+printResult(addAndHanble(2, 5, (n: number) => {
+    console.log(n);
+}))
