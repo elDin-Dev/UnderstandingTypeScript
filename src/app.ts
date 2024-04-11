34c91587855f205a3262ee63a723d39f63f7c37f
sandina <alejandro.sandin@roche.com>
2024-04-11 17:01:31 +0200

    constructor private public

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,48 @@
 class Department {
-    name: string;
+  private employees: string[] = [];
+  /*
+  private name: string;
+  
 
-    constructor(n: string) {
-        this.name = n;
-    }
+  constructor(n: string) {
+    this.name = n;
+  }
+  */
 
-    // Adding here as a hint 'this:Department' we ensure that typescript
-    // is going to help us avoid error with 'this hell'
-    describe(this:Department) {
-        console.log('Department: ' + this.name);
-    }
-    describeNoParm() {
-        console.log('Department: ' + this.name);
-    }
+  constructor(private id: string, private name: string) {}
+  // Adding here as a hint 'this:Department' we ensure that typescript
+  // is going to help us avoid error with 'this hell'
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+  describeNoParm() {
+    console.log("Department: " + this.name);
+  }
 
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
 
+  printEmployeeInformation(this: Department) {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('1', "Accounting");
+accounting.addEmployee('max');
+accounting.addEmployee('alex');
 
 accounting.describe();
+accounting.printEmployeeInformation();
 
 //Error because of the 'this' badusage
 
-const copyDept: { desc: Function } = { desc: accounting.describeNoParm };
+//const copyDept: { desc: Function } = { desc: accounting.describeNoParm };
 
-const copyDept2:{desc:Function} = { desc:accounting.describe };
+//const copyDept2: { desc: Function } = { desc: accounting.describe };
 
-
-console.log("this tipycal error: " + copyDept.desc())
+//console.log("this tipycal error: " + copyDept.desc());
 
 //Error because the object where we execute describe doesn't have a field called 'name
-console.log("this without error: "+ copycopyDept2Dept.describe())
-
+//console.log("this without error: " + copycopyDept2Dept.describe());
